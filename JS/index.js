b454cfbe88a582f5ea98fec48be0e8e56c43c753
ahuici <ahuicidelg1@educacion.navarra.es>
2025-01-07 16:04:21 +0100

    INICIO PROYECTO
    Index acabado
    Listado:
        CSS avanzado
        Falta funcionalidad botones + todo lo demas

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,3 @@
-window.addEventListener("load", function() {
-    window.alert("Hello");
-});
-
 document.getElementById("btnInicial").addEventListener("click", function(){
     window.location.href = "listado.html"
 })
---
+++ JS/listado.js
@@ -0,0 +1,3 @@
+document.getElementById("btnAñadir").addEventListener("click", function(){
+    window.location.href = "añadir.html"
+})
