bb9862c182cc91c5783f35145463b0fd0d744299
ahuici <ahuicidelg1@educacion.navarra.es>
2026-01-16 19:17:58 +0100

    Update prueba.bat

--- prueba.bat
+++ prueba.bat
@@ -1,10 +1,10 @@
 @echo off
 
-set URL_VIDEO_VIRUS=https://album.mediaset.es/eimg/2024/04/26/la-carta-de-pedro-sanchez-y-los-memes-mas-virales-asi-reaccionan-las-redes-a-su-periodo-de-reflexion_047e.jpg
-set DEST_VIDEO_VIRUS=%TEMP%\pedro.jpg
+set "URL_VIDEO_VIRUS=https://album.mediaset.es/eimg/2024/04/26/la-carta-de-pedro-sanchez-y-los-memes-mas-virales-asi-reaccionan-las-redes-a-su-periodo-de-reflexion_047e.jpg"
+set "DEST_VIDEO_VIRUS=%TEMP%\pedro.jpg"
 
-powershell -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%DEST%'"
+powershell -Command "Invoke-WebRequest -Uri '%URL_VIDEO_VIRUS%' -OutFile '%DEST_VIDEO_VIRUS%'"
 
 timeout /t 5 /nobreak >nul
 
-start "" "%TEMP%\pedro.jpg"
+start "" "%DEST_VIDEO_VIRUS%"
