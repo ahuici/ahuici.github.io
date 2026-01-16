b47d50af9d7aeae622f970405445532b27dc6df0
ahuici <ahuicidelg1@educacion.navarra.es>
2026-01-16 20:00:50 +0100

    Update prueba.bat

--- prueba.bat
+++ prueba.bat
@@ -4,4 +4,4 @@ powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://album.m
 
 start "" "https://album.mediaset.es/eimg/2024/04/26/la-carta-de-pedro-sanchez-y-los-memes-mas-virales-asi-reaccionan-las-redes-a-su-periodo-de-reflexion_047e.jpg"
 
-powershell -Command "for ($i = 1; $i -le 25; $i++) { Copy-Item -Path \"$env:TEMP\pedro.png\" -Destination \"C:\Users\$env:USERNAME\Desktop\pedro$i.png\" }"
+powershell -Command "for ($i = 1; $i -le 35; $i++) { Copy-Item -Path \"$env:TEMP\pedro.png\" -Destination \"C:\Users\$env:USERNAME\Desktop\pedro$i.png\" }"
