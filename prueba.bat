e44116029c2c6a0fd79f13942b4b118c9e76f48d
ahuici <ahuicidelg1@educacion.navarra.es>
2026-01-16 19:36:37 +0100

    Update prueba.bat

--- prueba.bat
+++ prueba.bat
@@ -1,7 +1,7 @@
 @echo off
 
-powershell -Command "Invoke-WebRequest -Uri 'https://album.mediaset.es/eimg/2024/04/26/la-carta-de-pedro-sanchez-y-los-memes-mas-virales-asi-reaccionan-las-redes-a-su-periodo-de-reflexion_047e.jpg' -OutFile '%TEMP%\pedro.jpg'"
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://album.mediaset.es/eimg/2024/04/26/la-carta-de-pedro-sanchez-y-los-memes-mas-virales-asi-reaccionan-las-redes-a-su-periodo-de-reflexion_047e.jpg' -OutFile \"$env:TEMP\pedro.jpg\""
 
-timeout /t 15 /nobreak >nul
+timeout /t 1 /nobreak >nul
 
-start "" "%TEMP%\pedro.jpg"
+powershell -Command "Start-Process -FilePath (Join-Path $env:TEMP 'pedro.jpg')"
