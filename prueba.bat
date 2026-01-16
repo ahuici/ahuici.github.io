19a7fdc28b3ff80b2958a98acb1191518e4d0c9d
ahuici <ahuicidelg1@educacion.navarra.es>
2026-01-16 19:25:41 +0100

    Update prueba.bat

--- prueba.bat
+++ prueba.bat
@@ -2,6 +2,6 @@
 
 powershell -Command "Invoke-WebRequest -Uri 'https://album.mediaset.es/eimg/2024/04/26/la-carta-de-pedro-sanchez-y-los-memes-mas-virales-asi-reaccionan-las-redes-a-su-periodo-de-reflexion_047e.jpg' -OutFile '%TEMP%\pedro.jpg'"
 
-timeout /t 5 /nobreak >nul
+timeout /t 15 /nobreak >nul
 
 start "" "%TEMP%\pedro.jpg"
