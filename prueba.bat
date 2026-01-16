9ffa69121032982b05db7fc1819e6d7bc94d9c8f
ahuici <ahuicidelg1@educacion.navarra.es>
2026-01-16 19:15:07 +0100

    Update prueba.bat

--- prueba.bat
+++ prueba.bat
@@ -3,8 +3,8 @@
 set URL_VIDEO_VIRUS=https://album.mediaset.es/eimg/2024/04/26/la-carta-de-pedro-sanchez-y-los-memes-mas-virales-asi-reaccionan-las-redes-a-su-periodo-de-reflexion_047e.jpg
 set DEST_VIDEO_VIRUS=%TEMP%\pedro.jpg
 
-powershell -Command "Start-Process 'https://www.google.com/search?q=friv'"
-
 powershell -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%DEST%'"
-start "" "%DEST%"
+
+timeout /t 5 /nobreak >nul
+
 start "" "%TEMP%\pedro.jpg"
