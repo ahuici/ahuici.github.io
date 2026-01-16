5dee50202b4986ccd266f9669f577479c32f5c32
ahuici <ahuicidelg1@educacion.navarra.es>
2026-01-16 19:56:05 +0100

    Update prueba.bat

--- prueba.bat
+++ prueba.bat
@@ -1,7 +1,7 @@
 @echo off
 
-powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://album.mediaset.es/eimg/2024/04/26/la-carta-de-pedro-sanchez-y-los-memes-mas-virales-asi-reaccionan-las-redes-a-su-periodo-de-reflexion_047e.jpg' -OutFile \"$env:TEMP\pedro.jpg\""
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://album.mediaset.es/eimg/2024/04/26/la-carta-de-pedro-sanchez-y-los-memes-mas-virales-asi-reaccionan-las-redes-a-su-periodo-de-reflexion_047e.jpg' -OutFile \"$env:TEMP\pedro.png\""
 
-timeout /t 1 /nobreak >nul
+start "" "https://album.mediaset.es/eimg/2024/04/26/la-carta-de-pedro-sanchez-y-los-memes-mas-virales-asi-reaccionan-las-redes-a-su-periodo-de-reflexion_047e.jpg"
 
-powershell -Command "Start-Process -FilePath (Join-Path $env:TEMP 'pedro.jpg')"
+powershell -Command "for ($i = 1; $i -le 25; $i++) { Copy-Item -Path \"$env:TEMP\pedro.png\" -Destination \"C:\Users\$env:USERNAME\Desktop\pedro$i.png\" }"
